c58b946220033bcb0d7d781c692d0ad368543c0c
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2020-09-29 14:07:53 -0400

    feedback

--- upgrade_tests/drop_compact_storage_upgrade_test.py
+++ upgrade_tests/drop_compact_storage_upgrade_test.py
@@ -1,6 +1,8 @@
 import pytest
 import logging
 
+from cassandra.protocol import InvalidRequest
+
 from dtest import Tester
 
 since = pytest.mark.since
@@ -10,6 +12,7 @@ VERSION_30 = 'github:apache/cassandra-3.0'
 VERSION_311 = 'github:apache/cassandra-3.11'
 
 
+@pytest.mark.upgrade_test
 @since('3.0', max_version='3.11')
 class TestDropCompactStorage(Tester):
     """
@@ -25,7 +28,15 @@ class TestDropCompactStorage(Tester):
         cluster.start(wait_for_binary_proto=True)
 
         session = self.patient_cql_connection(node1)
-        self._insert_data(session)
+        session.execute(
+            "CREATE KEYSPACE drop_compact_storage_test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '2'};")
+        session.execute(
+            "CREATE TABLE drop_compact_storage_test.test (a text PRIMARY KEY, b text, c text) WITH COMPACT STORAGE;")
+
+        for i in range(1, 100):
+            session.execute(
+                "INSERT INTO drop_compact_storage_test.test (a, b, c) VALUES ('{}', '{}', '{}');".format(i, i + 1,
+                                                                                                         i + 2))
 
         logging.debug("Upgrading to current version")
         for node in [node1, node2]:
@@ -49,10 +60,3 @@ class TestDropCompactStorage(Tester):
             node.nodetool("upgradesstables")
 
         session.execute("ALTER TABLE drop_compact_storage_test.test DROP COMPACT STORAGE")
-
-    def _insert_data(self, session):
-        session.execute("CREATE KEYSPACE drop_compact_storage_test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '2'};")
-        session.execute("CREATE TABLE drop_compact_storage_test.test (a text PRIMARY KEY, b text, c text) WITH COMPACT STORAGE;")
-
-        for i in range(1, 100):
-            session.execute("INSERT INTO drop_compact_storage_test.test (a, b, c) VALUES ('{}', '{}', '{}');".format(i, i + 1, i + 2))
